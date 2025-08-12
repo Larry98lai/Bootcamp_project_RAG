7600cc90e8e8266ca0046a47017ee1a6f4ff914f
Larry98lai <larrylai1998@gmail.com>
2025-08-12 03:45:56 +0000

    edit the content

--- welcome.py
+++ welcome.py
@@ -2,13 +2,10 @@
 import streamlit as st
 
 st.title("About this App")
-st.write("This is a streamlit app that demonstrates how to use the OpenAI API to generate text completions.")
+st.write("This is a streamlit app that act as a bot companion to help users relieve/ manage stress.")
 
 with st.expander("How to use this App properly?"):
     st.markdown("""
     - Use the sidebar to navigate between pages.
-    - On the **Main Page**, enter your query about courses (e.g., "I want to learn data science").
-    - The app will detect relevant course categories and recommend suitable options.
-    - You can explore additional pages like `Page 2` for browsing or filtering course catalogs.
-    - Data is sourced from curated JSON files and AI-enhanced search.
+    - On the **Main Page**, enter what you want to share with the bot.
     """)
